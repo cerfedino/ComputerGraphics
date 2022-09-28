ece9fc471456d6af42d1879500297b1785bcfba1
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-09-28 19:53:17 +0200

    Updated render script

--- Assignment1/render.sh
+++ Assignment1/render.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [[ $1 -eq "--render" ]]
 then
-  g++ src/main.cpp && ./a.out && ffmpeg -i "result.ppm" "result.png" && (xdg-open result.png &> /dev/null || open result.png &> /dev/null)
+  g++ src/main.cpp && ./a.out && ffmpeg -y -i "result.ppm" "result.png" && (xdg-open result.png &> /dev/null || open result.png &> /dev/null)
 else
-  g++ src/main.cpp && ./a.out && ffmpeg -i "result.ppm" "result.png"
+  g++ src/main.cpp && ./a.out && ffmpeg -y -i "result.ppm" "result.png"
 fi
