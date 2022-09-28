8b0de4d8dbf0d90c224a79bfffb156de4fe34870
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-09-28 11:36:46 +0200

    Ignore intersections behind camera, optimization

--- Assignment 1/src/main.cpp
+++ Assignment 1/src/main.cpp
@@ -71,32 +71,48 @@ public:
     Sphere(float radius, glm::vec3 center, glm::vec3 color) : radius(radius), center(center){
 		this->color = color;
     }
-
 	/** Implementation of the intersection function*/
-    Hit intersect(Ray ray){
-        const float delta = glm::sqrt(
-                glm::pow(glm::length(center), 2)
-                -glm::pow(glm::dot(center, ray.direction),2)
-                );
-        float closest_t;
-        // No intersection
-        if(delta > radius) {
-            Hit hit;
-            hit.hit = false;
-            return hit;
-        } else if (delta < radius) {
-            float t1 = glm::dot(center, ray.direction) + glm::sqrt(glm::pow(radius,2)-glm::pow(delta,2));
-            float t2 = glm::dot(center, ray.direction) - glm::sqrt(glm::pow(radius,2)-glm::pow(delta,2));
-            closest_t = glm::min(t1,t2);
-        } else {
-            closest_t = glm::dot(center, ray.direction) + glm::sqrt(glm::pow(radius,2)-glm::pow(delta,2));
-        }
-        Hit hit;
-        hit.hit = true;
-        hit.distance = closest_t;
-        hit.intersection = ray.direction * closest_t;
-        hit.normal = glm::normalize(hit.intersection - center);
-        hit.object = this;
+    Hit intersect(Ray ray) {
+		
+		Hit hit;
+		hit.hit = false; // by default, there is no intersection
+
+		const glm::vec3 c = center - ray.origin; // center of the sphere in the coordinate system of the ray
+		
+		const float a = glm::dot(c, ray.direction);
+
+		const float D_squared = glm::dot(c, c) - a*a;
+		const float D = sqrt(D_squared); // distance from the center of the sphere to the ray
+
+		float closest_t;
+		// No intersection
+		if(D > radius) { 
+			return hit;
+
+		// Two intersections
+		} else if (D < radius) { 
+			// Distance of first point (always the closest)
+			const float t1 = a - sqrt(radius*radius - D_squared);
+			// float t2 = a + sqrt(radius*radius - D_squared);
+			
+			// We only get the smallest positive between the two points
+			closest_t = t1 >= 0 ? t1 : sqrt(radius*radius - D_squared);
+
+		// One intersection
+		} else {
+			closest_t = a + sqrt(radius*radius - D_squared);
+		}
+
+		// We ignore intersection points behind the camera
+		if(closest_t < 0) {
+			return hit;
+		}
+	
+		hit.hit = true;
+		hit.distance = closest_t;
+		hit.intersection = ray.direction * closest_t;
+		hit.normal = glm::normalize(hit.intersection - center);
+		hit.object = this;
 
 		return hit;
     }
@@ -110,7 +126,7 @@ vector<Object *> objects; ///< A list of all objects in the scene
  @param ray Ray that should be traced through the scene
  @return Color at the intersection point
  */
-glm::vec3 trace_ray(Ray ray) {
+glm::vec3 trace_ray(Ray ray){
 	
 	// hit structure representing the closest intersection
 	Hit closest_hit;
@@ -119,17 +135,16 @@ glm::vec3 trace_ray(Ray ray) {
 	closest_hit.distance = INFINITY;
 	
 	//Loop over all objects to find the closest intersection
-	for(int k = 0; k<objects.size(); k++){
+	for (int k = 0; k<objects.size(); k++) {
 		Hit hit = objects[k]->intersect(ray);
-		if(hit.hit == true && hit.distance < closest_hit.distance) {
-            closest_hit = hit;
-        }
+		if (hit.hit == true && hit.distance < closest_hit.distance)
+			closest_hit = hit;
 	}
 	
 	glm::vec3 color;
-	if(closest_hit.hit){
+	if (closest_hit.hit) {
 		color = closest_hit.object->color;
-	}else{
+	} else {
 		color = glm::vec3(0.0, 0.0, 0.0);
 	}
 	return color;
@@ -138,8 +153,11 @@ glm::vec3 trace_ray(Ray ray) {
  Function defining the scene
  */
 void sceneDefinition (){
-    objects.push_back(new Sphere(1.0, glm::vec3(-0,-2,8), glm::vec3(0.6, 0.9, 0.6)));
-    objects.push_back(new Sphere(1.0, glm::vec3(1.0, -2.0, 8.0), glm::vec3(0.6, 0.6, 0.9)));
+	// first sphere (Exercise 1)
+	objects.push_back(new Sphere(1.0, glm::vec3(-0,-2,8), glm::vec3(0.6, 0.9, 0.6)));
+	
+	// second sphere (Exercise 4)
+	objects.push_back(new Sphere(1.0, glm::vec3(1.0, -2.0, 8.0), glm::vec3(0.6, 0.6, 0.9)));
 }
 
 int main(int argc, const char * argv[]) {
@@ -153,55 +171,38 @@ int main(int argc, const char * argv[]) {
 	sceneDefinition(); // Let's define the scene
 	
 	Image image(width,height); // Create an image where we will store the result
-
-	// Parametrizing with plane
-	const float s = (2*glm::tan(fov/2))/width;
-
-	const float X = (-width*s)/2;
-	const float Y = (height*s)/2;
+	
+	const float s =(2 * tan(fov/2)) / width; // size of one pixel
+	const float X = -(width * s) / 2; // X coordinate of the first pixel
+	const float Y = (height * s) / 2; // Y coordinate of the first pixel
     const float Z = 1;
+	glm::vec3 origin = glm::vec3(0, 0, 0); // origin of the camera
 
-    /* -------------------------------------------------
-	 
-	Place for your code: Loop over pixels to form and traverse the rays through the scene
-	 
-	------------------------------------------------- */
-
-    const glm::vec3 origin = glm::vec3(0,0,0);
-
-    for(int i = 0; i < width ; i++)
-        for(int j = 0; j < height ; j++) {
-
-            float dx = X + (i*s) + (s/2);
-            float dy = Y - (j*s) - (s/2);
-            glm::vec3 direction = glm::normalize(glm::vec3(dx, dy, Z));
-
-            Ray ray(origin, direction);
-            image.setPixel(i, j, trace_ray(ray));
-			/*
-			 
-			Place for your code: ray definition for pixel (i,j), ray traversal
-			 
-			*/
+	// Loop over all pixels and traverce the rays through the scene
+    for (int i = 0; i < width ; i++) {
+        for (int j = 0; j < height ; j++) {
 			
-			//Definition of the ray
-			//glm::vec3 origin(0, 0, 0);
-            //glm::vec3 direction(?, ?, ?);               // fill in the correct values
-            //direction = glm::normalize(direction);
-            
-            //Ray ray(origin, direction);  // ray traversal
-			
-			//image.setPixel(i, j, trace_ray(ray));
+			// X coordinate of the current pixel
+			const float dx = X + i*s + 0.5*s;
+			// Y coordinate of the current pixel
+			const float dy = Y - j*s - 0.5*s; 
+
+			// direction of the ray
+			glm::vec3 direction = glm::normalize(glm::vec3(dx, dy, Z)); 
+			// create the ray
+			Ray ray(origin, direction); 
+			// trace the ray and set the color of the pixel
+			image.setPixel(i, j, trace_ray(ray)); 
         }
-    
+	}
     t = clock() - t;
     cout<<"It took " << ((float)t)/CLOCKS_PER_SEC<< " seconds to render the image."<< endl;
     cout<<"I could render at "<< (float)CLOCKS_PER_SEC/((float)t) << " frames per second."<<endl;
     
 	// Writing the final results of the rendering
-	if (argc == 2){
+	if (argc == 2) {
 		image.writeImage(argv[2]);
-	}else{
+	} else {
 		image.writeImage("./result.ppm");
 	}
 	
