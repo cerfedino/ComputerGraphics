f3f544bcb799e3ab2bea81b9dc3604e5134c6cd6
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-10-06 23:55:39 +0200

    Further updated rendering scripts

--- Assignment1/render.sh
+++ Assignment1/render.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir ./render &> /dev/null
 if [[ $1 -eq "--open" ]]
 then
   g++ src/main.cpp && ./a.out && ffmpeg -y -i "render/result.ppm" "render/result.png"&> /dev/null && (xdg-open render/result.png &> /dev/null || open render/result.png &> /dev/null)
