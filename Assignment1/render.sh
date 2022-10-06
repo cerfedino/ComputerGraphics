8eeda04ecb3712bdd27787d56c41a284c5fad292
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-10-06 23:21:16 +0200

    Updated render scripts

--- Assignment1/render.sh
+++ Assignment1/render.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [[ $1 -eq "--render" ]]
+if [[ $1 -eq "--open" ]]
 then
   g++ src/main.cpp && ./a.out && ffmpeg -y -i "result.ppm" "result.png"&> /dev/null && (xdg-open result.png &> /dev/null || open result.png &> /dev/null)
 else
--- Assignment2/render.sh
+++ Assignment2/render.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [[ $1 -eq "--render" ]]
+if [[ $1 -eq "--open" ]]
 then
-  g++ src/main.cpp && ./a.out && ffmpeg -y -i "result.ppm" "result.png"&> /dev/null && (xdg-open result.png &> /dev/null || open result.png &> /dev/null)
+  g++ src/main.cpp && ./a.out && ffmpeg -y -i "render/result.ppm" "render/result.png"&> /dev/null && (xdg-open render/result.png &> /dev/null || open render/result.png &> /dev/null)
 else
-  g++ src/main.cpp && ./a.out && ffmpeg -y -i "result.ppm" "result.png"&> /dev/null
+  g++ src/main.cpp && ./a.out && ffmpeg -y -i "render/result.ppm" "render/result.png"&> /dev/null
 fi
