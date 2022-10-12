04053e387fca4fdf27b2418e2c350cdfbeac92a5
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-10-12 16:03:26 +0200

    Integrated with materials from ex3

--- Assignment3/src/main.cpp
+++ Assignment3/src/main.cpp
@@ -89,53 +89,59 @@ public:
 		this->material = material;
 	}
 	/** Implementation of the intersection function*/
-    Hit intersect(Ray ray){
+    Hit intersect(Ray ray) {
 
-        glm::vec3 c = center - ray.origin;
+		Hit hit;
+		hit.hit = false; // by default, there is no intersection
 
-        float cdotc = glm::dot(c,c);
-        float cdotd = glm::dot(c, ray.direction);
+		const glm::vec3 c = center - ray.origin; // center of the sphere in the coordinate system of the ray
 
-        Hit hit;
+		const float a = glm::dot(c, ray.direction);
 
-        float D = 0;
-		if (cdotc > cdotd*cdotd){
-			D =  sqrt(cdotc - cdotd*cdotd);
+		const float D_squared = glm::dot(c, c) - a*a;
+		const float D = sqrt(D_squared); // distance from the center of the sphere to the ray
+
+		float closest_t;
+		// No intersection
+		if(D > radius) {
+			return hit;
+
+		// Two intersections
+		} else if (D < radius) {
+			// Distance of first point (always the closest)
+			const float t1 = a - sqrt(radius*radius - D_squared);
+			// float t2 = a + sqrt(radius*radius - D_squared);
+
+			// We only get the smallest positive between the two points
+			closest_t = t1 >= 0 ? t1 : a + sqrt(radius*radius - D_squared);
+
+		// One intersection
+		} else {
+			closest_t = a + sqrt(radius*radius - D_squared);
 		}
-        if(D<=radius){
-            hit.hit = true;
-            float t1 = cdotd - sqrt(radius*radius - D*D);
-            float t2 = cdotd + sqrt(radius*radius - D*D);
 
-            float t = t1;
-            if(t<0) t = t2;
-            if(t<0){
-                hit.hit = false;
-                return hit;
-            }
-
-			hit.intersection = ray.origin + t * ray.direction;
-			hit.normal = glm::normalize(hit.intersection - center);
-			hit.distance = glm::distance(ray.origin, hit.intersection);
-			hit.object = this;
-			
-			
-			/*
-			 
-			 
-			 Excercise 2 - computing texture coordintes for the sphere. You can refer to them as s and t, or x and y, whatever you prefer ;)
-			 
-			 hit.uv.s =
-			 hit.uv.t =
-			 
-			 
-			 
-			 */
-
-        }
-		else{
-            hit.hit = false;
+		// We ignore intersection points behind the camera
+		if(closest_t < 0) {
+			return hit;
 		}
+
+		hit.hit = true;
+		hit.distance = closest_t;
+		hit.intersection = ray.origin + ray.direction * hit.distance;
+		hit.normal = glm::normalize(hit.intersection - center);
+		hit.object = this;
+
+        /*
+
+
+         Exercise 2 - computing texture coordinates for the sphere. You can refer to them as s and t, or x and y, whatever you prefer ;)
+
+         hit.uv.s =
+         hit.uv.t =
+
+
+
+         */
 		return hit;
     }
 };
@@ -154,25 +160,20 @@ public:
 		this->material = material;
 	}
 	Hit intersect(Ray ray){
-		
+
 		Hit hit;
 		hit.hit = false;
-		
-		/*
-		
-		 Excercise 1 - Plane-ray intersection
-		 
-		 */
 
-		float t = glm::dot(point - ray.origin, normal) / glm::dot(ray.direction, normal);
-		
-		if (t > 0) {
-			hit.hit = true;
-			hit.intersection = ray.origin + t * ray.direction;
-			hit.normal = glm::normalize(normal);
-			hit.distance = glm::distance(ray.origin, hit.intersection);
-			hit.object = this;
-		}
+		/*
+
+
+
+		 Excercise 1 - Plane-ray intersection
+
+
+
+
+		 */
 
 		return hit;
 	}
@@ -185,7 +186,7 @@ public:
 class Light{
 public:
 	glm::vec3 position; ///< Position of the light source
-	glm::vec3 color; ///< Color/intentisty of the light source
+	glm::vec3 color; ///< Color/intensity of the light source
 	Light(glm::vec3 position): position(position){
 		color = glm::vec3(1.0);
 	}
@@ -199,7 +200,7 @@ vector<Object *> objects; ///< A list of all objects in the scene
 
 
 /** Function for computing color of an object according to the Phong Model
- @param point A point belonging to the object for which the color is computer
+ @param point A point belonging to the object for which the color is computed
  @param normal A normal vector the the point
  @param uv Texture coordinates
  @param view_direction A normalized direction from the point to the viewer/camera
@@ -208,54 +209,35 @@ vector<Object *> objects; ///< A list of all objects in the scene
 glm::vec3 PhongModel(glm::vec3 point, glm::vec3 normal, glm::vec2 uv, glm::vec3 view_direction, Material material){
 
 	glm::vec3 color(0.0);
-	for(int light_num = 0; light_num < lights.size(); light_num++){
 
-		glm::vec3 light_direction = glm::normalize(lights[light_num]->position - point);
-		glm::vec3 reflected_direction = glm::reflect(-light_direction, normal);
+    for(Light* light : lights) {
+        glm::vec3 l = glm::normalize(light->position - point); // direction from the point to the light source
+        /*
 
-		float NdotL = glm::clamp(glm::dot(normal, light_direction), 0.0f, 1.0f);
-		float VdotR = glm::clamp(glm::dot(view_direction, reflected_direction), 0.0f, 1.0f);
+         Exercise 2 - Modify the code by adding texturing, i.e., the diffuse color should be computed using one of the texture functions according to the texture coordinates stored in the uv variable. Make sure that the code works also for objects that should not have texture.
 
-		
-		/*
-		 
-		 
-		 Excercise 2 - Modify the code by adding texturing, 
-		 i.e., the diffuse color should be computed using one of the texture functions according to the texture coordinates stored in the uv variable. 
-		 Make sure that the code works also for objects that should not have texture.
-		 
-		 
-		 */
-
-		// map texture to object
-		if (material.texture != NULL) {
-			color += material.texture->getPixel(uv.s, uv.t) * NdotL * lights[light_num]->color;
-		}
-		else {
-			color += material.diffuse_color * NdotL * lights[light_num]->color;
-		}
+         */
 
 
+        float diffuse = glm::dot(l, normal);
+        if (diffuse < 0) diffuse = 0;
 
-		
-		glm::vec3 diffuse = material.diffuse * glm::vec3(NdotL);
-		glm::vec3 specular = material.specular * glm::vec3(pow(VdotR, material.shininess));
-		
-		
-		/*
-		 
-		 
-		 Excercise 3 - Modify the code by adding attenuation of the light due to distance from the intersection point to the light source
-		 
-		 
-		 
-		 */
-		
+        glm::vec3 h = glm::normalize(l + view_direction); // half vector
+        float specular = glm::pow(glm::dot(h,normal), 4*material.shininess);
+        if (specular < 0) specular = 0;
 
-		color += lights[light_num]->color * (diffuse + specular);
-	}
-	color += ambient_light * material.ambient;
+        color += light->color * (material.diffuse*diffuse + material.specular*specular);
+    }
 
+    /*
+
+     Exercise 3 - Modify the code by adding attenuation of the light due to distance from the intersection point to the light source
+
+     */
+
+    color += ambient_light*material.ambient;
+
+	// The final color has to be clamped so the values do not go beyond 0 and 1.
 	color = glm::clamp(color, glm::vec3(0.0), glm::vec3(1.0));
 	return color;
 }
@@ -267,22 +249,24 @@ glm::vec3 PhongModel(glm::vec3 point, glm::vec3 normal, glm::vec2 uv, glm::vec3 
  */
 glm::vec3 trace_ray(Ray ray){
 
+	// hit structure representing the closest intersection
 	Hit closest_hit;
 
 	closest_hit.hit = false;
 	closest_hit.distance = INFINITY;
 
-	for(int k = 0; k<objects.size(); k++){
+	//Loop over all objects to find the closest intersection
+	for (int k = 0; k<objects.size(); k++) {
 		Hit hit = objects[k]->intersect(ray);
-		if(hit.hit == true && hit.distance < closest_hit.distance)
+		if (hit.hit == true && hit.distance < closest_hit.distance)
 			closest_hit = hit;
 	}
 
 	glm::vec3 color(0.0);
 
-	if(closest_hit.hit){
-		color = PhongModel(closest_hit.intersection, closest_hit.normal, closest_hit.uv, glm::normalize(-ray.direction), closest_hit.object->getMaterial());
-	}else{
+	if (closest_hit.hit) {
+        color = PhongModel(closest_hit.intersection, closest_hit.normal, closest_hit.uv, glm::normalize(-ray.direction), closest_hit.object->getMaterial());
+	} else {
 		color = glm::vec3(0.0, 0.0, 0.0);
 	}
 	return color;
@@ -290,61 +274,52 @@ glm::vec3 trace_ray(Ray ray){
 /**
  Function defining the scene
  */
-void sceneDefinition (){
+void sceneDefinition() {
 
-	Material green_diffuse;
-	green_diffuse.ambient = glm::vec3(0.07f, 0.09f, 0.07f);
-	green_diffuse.diffuse = glm::vec3(0.7f, 0.9f, 0.7f);
+    // materials
+    Material blue;
+    blue.ambient = glm::vec3(0.07f, 0.07f, 0.1f);
+    blue.diffuse = glm::vec3(0.7f, 0.7f, 1.0f);
+    blue.specular = glm::vec3(0.6);
+    blue.shininess = 100.0;
 
-	Material red_specular;
-	red_specular.diffuse = glm::vec3(1.0f, 0.3f, 0.3f);
-	red_specular.ambient = glm::vec3(0.01f, 0.03f, 0.03f);
-	red_specular.specular = glm::vec3(0.5);
-	red_specular.shininess = 10.0;
+    Material red;
+    red.ambient = glm::vec3(0.01f, 0.03f, 0.03f);
+    red.diffuse = glm::vec3(1.0f, 0.3f, 0.3f);
+    red.specular = glm::vec3(0.5);
+    red.shininess = 10.0;
 
-	Material blue_specular;
-	blue_specular.ambient = glm::vec3(0.07f, 0.07f, 0.1f);
-	blue_specular.diffuse = glm::vec3(0.7f, 0.7f, 1.0f);
-	blue_specular.specular = glm::vec3(0.6);
-	blue_specular.shininess = 100.0;
+    Material green;
+    green.ambient = glm::vec3(0.07f, 0.09f, 0.07f);
+    green.diffuse = glm::vec3(0.7f, 0.9f, 0.7f);
+    green.specular = glm::vec3(0.0);
+    green.shininess = 0.0;
 
 
-	objects.push_back(new Sphere(1.0, glm::vec3(1,-2,8), blue_specular));
-	objects.push_back(new Sphere(0.5, glm::vec3(-1,-2.5,6), red_specular));
-	objects.push_back(new Sphere(1.0, glm::vec3(3,-2,6), green_diffuse));
-	
-	
-	// Excercise 2 - Textured sphere
-	Material textured;
-	textured.texture = &checkerboardTexture;
-	objects.push_back(new Sphere(7.0, glm::vec3(-6,4,23), textured));
-	
-	
-	
-	/*
-	 
-	 
-	 Excercise 1 - Definition of planes and the materials
-	 
-	 
-	 
-	 */
-	// add six planes to form a cube
-	// y
-	objects.push_back(new Plane(glm::vec3(0,-3,0), glm::vec3(0,1,0), red_specular));
-	objects.push_back(new Plane(glm::vec3(0,27,0), glm::vec3(0,-1,0), blue_specular));
-	// x
-	objects.push_back(new Plane(glm::vec3(-15,0,0), glm::vec3(1,0,0), green_diffuse));
-	objects.push_back(new Plane(glm::vec3(15,0,0), glm::vec3(-1,0,0), red_specular));
-	// z
-	objects.push_back(new Plane(glm::vec3(0,0,-0.01), glm::vec3(0,0,-1), green_diffuse));
-	objects.push_back(new Plane(glm::vec3(0,0,30), glm::vec3(0,0,1), green_diffuse));
+    // objects
+    objects.push_back(new Sphere(1.0, glm::vec3(1.0, -2.0, 8.0), blue));
+    objects.push_back(new Sphere(0.5, glm::vec3(-1.0, -2.5, 6.0), red));
+    objects.push_back(new Sphere(1.0, glm::vec3(3.0, -2.0, 6.0), green));
 
-	
-	lights.push_back(new Light(glm::vec3(0, 26, 5), glm::vec3(0.4)));
-	lights.push_back(new Light(glm::vec3(0, 1, 12), glm::vec3(0.4)));
-	lights.push_back(new Light(glm::vec3(0, 5, 1), glm::vec3(0.4)));
+    // Exercise 2 - Textured sphere
+    //Material textured;
+    //textured.texture = &checkerboardTexture;
+    //objects.push_back(new Sphere(7.0, glm::vec3(-6,4,23), textured));
+
+
+
+    /*
+
+     Exercise 1 - Definition of planes and the materials
+
+     */
+
+    // lights
+    lights.push_back(new Light(glm::vec3(0.0, 26.0, 5.0), glm::vec3(0.4)));
+    lights.push_back(new Light(glm::vec3(0.0, 1.0, 12.0), glm::vec3(0.4)));
+    lights.push_back(new Light(glm::vec3(0.0, 5.0, 1.0), glm::vec3(0.4)));
 }
+
 
 /**
  Function performing tonemapping of the intensities computed using the raytracer
@@ -352,62 +327,62 @@ void sceneDefinition (){
  @return Tonemapped intensity in range (0,1)
  */
 glm::vec3 toneMapping(glm::vec3 intensity){
-	
-	glm::vec3 tonemapped = intensity; //tonemapped intensity
-	
-	/*
-	 
-	 
-	 Excercise 3 - Tone mapping
-	 
-	 
-	 
-	 */
-	
-	return glm::clamp(tonemapped, glm::vec3(0.0), glm::vec3(1.0));
+
+    glm::vec3 tonemapped = intensity; //tonemapped intensity
+
+    /*
+
+     Excercise 3 - Tone mapping
+
+     */
+
+    return glm::clamp(tonemapped, glm::vec3(0.0), glm::vec3(1.0));
 }
 
 int main(int argc, const char * argv[]) {
 
     clock_t t = clock(); // variable for keeping the time of the rendering
 
-    int width = 1024; //width of the image
-    int height = 768; // height of the image
+    int width = 1920; //width of the image
+    int height = 1080; // height of the image
     float fov = 90; // field of view
 
-	sceneDefinition(); // Let's define a scene
+	sceneDefinition(); // Let's define the scene
 
 	Image image(width,height); // Create an image where we will store the result
 
-    float s = 2*tan(0.5*fov/180*M_PI)/width;
-    float X = -s * width / 2;
-    float Y = s * height / 2;
+	const float s = 2*tan(0.5*fov/180*M_PI)/width;; // size of one pixel
+	const float X = -(width * s) / 2; // X coordinate of the first pixel
+	const float Y = (height * s) / 2; // Y coordinate of the first pixel
+    const float Z = 1;
+	glm::vec3 origin = glm::vec3(0.0, 0.0, 0.0); // origin of the camera
 
-    for(int i = 0; i < width ; i++)
-        for(int j = 0; j < height ; j++){
+	// Loop over all pixels and traverse the rays through the scene
+    for (int i = 0; i < width ; i++) {
+        for (int j = 0; j < height ; j++) {
 
-			float dx = X + i*s + s/2;
-            float dy = Y - j*s - s/2;
-            float dz = 1;
+			// X coordinate of the current pixel
+			const float dx = X + i*s + 0.5*s;
+			// Y coordinate of the current pixel
+			const float dy = Y - j*s - 0.5*s;
 
-			glm::vec3 origin(0, 0, 0);
-            glm::vec3 direction(dx, dy, dz);
-            direction = glm::normalize(direction);
-
-            Ray ray(origin, direction);
-
+			// direction of the ray
+			glm::vec3 direction = glm::normalize(glm::vec3(dx, dy, Z));
+			// create the ray
+			Ray ray(origin, direction);
+			// trace the ray and set the color of the pixel
 			image.setPixel(i, j, toneMapping(trace_ray(ray)));
         }
-
+	}
     t = clock() - t;
     cout<<"It took " << ((float)t)/CLOCKS_PER_SEC<< " seconds to render the image."<< endl;
     cout<<"I could render at "<< (float)CLOCKS_PER_SEC/((float)t) << " frames per second."<<endl;
 
 	// Writing the final results of the rendering
-	if (argc == 2){
-		image.writeImage(argv[1]);
-	}else{
-		image.writeImage("./result.ppm");
+	if (argc == 2) {
+		image.writeImage(argv[2]);
+	} else {
+		image.writeImage("./render/result.ppm");
 	}
 
     return 0;
