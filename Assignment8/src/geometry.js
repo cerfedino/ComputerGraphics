0678536b1e6c26ec1efe81f1e4ff4ceb31eeade8
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-11-14 11:53:43 +0100

    Added assignment8 template

---
+++ Assignment8/src/geometry.js
@@ -0,0 +1,10 @@
+//------------ Assignment ----------------
+// Add vertices for the cube as well as colors for corresponding vertices.
+// Remember that:
+// (1) The vertices for each triangle has to be provided in a counter-clockwise order to make the face culling work properly. If the order of vertices won't be correct the triangles won't be visible.
+// (2) The current coordinate system has: x-axis pointing right, y-axis pointing up, and z-axis pointing towards the screen
+// (3) Everything which is within [-1..1]x[-1..1]x[-1..1] volume will be projected onto the screen along z-axis
+//-----------------------------------------
+
+//var cube_vertices = [...];
+//var cube_colors = [...];
